6f628945b8c8bca65795816304355b5480b3f243
Ayoub El Hayat <ayoub.elhayat@aize.io>
2023-08-17 13:31:58 +0200

    feat(filters): add filter menu

--- src/app/filters/filters.component.ts
+++ src/app/filters/filters.component.ts
@@ -1,12 +1,14 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ApiService } from '../api.service';
 import { FiltersService } from './filters.service';
 import { FiltersType } from '../types';
 import { Pipe, PipeTransform } from '@angular/core';
-import { map } from 'rxjs';
+import { map, tap } from 'rxjs';
 import { CalendarComponent, MultiSelectComponent, SliderComponent } from '@aize/ui';
+import { Overlay, OverlayModule, ScrollStrategy } from '@angular/cdk/overlay';
+import { SelectionModel } from '@angular/cdk/collections';
 
 
 @Pipe({
@@ -27,17 +29,31 @@ export class ActiveFiltersPipe implements PipeTransform {
 @Component({
   selector: 'aize-filters',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, ActiveFiltersPipe, MultiSelectComponent, SliderComponent, CalendarComponent],
+  imports: [CommonModule, OverlayModule, HttpClientModule, ActiveFiltersPipe, MultiSelectComponent, SliderComponent, CalendarComponent],
   providers: [ApiService, FiltersService],
   templateUrl: './filters.component.html',
   styleUrls: ['./filters.component.scss'],
 })
-export class FiltersComponent {
+export class FiltersComponent implements OnInit {
+  private readonly overlay = inject(Overlay);
+  private defaultFilters: Array<keyof FiltersType> = ['System', 'Discipline', 'FIRE AREA', 'CALIBRATED RANGE MAX', 'CALIBRATED RANGE MAX_UOM', 'createdTime'];
+
+  protected availableFilters: Array<keyof FiltersType> = [];
   protected readonly filters$ = inject(FiltersService).getFilters().pipe(
+    tap((filters) => {
+      this.availableFilters = Object.keys(filters) as Array<keyof FiltersType>;
+    }),
     map(filters => Object.entries(filters))
   );
-  protected defaultFilters: Array<keyof FiltersType> = ['System', 'Discipline', 'FIRE AREA', 'CALIBRATED RANGE MAX', 'CALIBRATED RANGE MAX_UOM', 'createdTime'];
+  protected readonly selectionModel = new SelectionModel<keyof FiltersType>(true);
+  protected scrollStrategy!: ScrollStrategy;
 
+  isDropdownOpen = false;
+
+  ngOnInit(): void {
+    this.scrollStrategy = this.overlay.scrollStrategies.reposition();
+    this.selectionModel.select(...this.defaultFilters)
+  }
   onSelectionChange(values: Array<string>): void {
     console.table(values)
   }
